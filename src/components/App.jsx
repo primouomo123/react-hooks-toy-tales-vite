a347ad53a3485aef8ff20b7876333c1f402ce106
Luis Rios Alvarado <luismanuelrios123@gmail.com>
2025-10-16 00:03:52 -0400

    Finished the GET request and toys are properly displayed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
 import ToyContainer from "./ToyContainer";
 
 function App() {
+  const [toys, setToys] = useState([]);
   const [showForm, setShowForm] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("http://localhost:3001/toys")
+    .then(r => {
+      if (r.ok) {
+        return r.json();
+      } else {throw new Error("Failed to get toys");
+      }
+    })
+    .then(setToys)
+    .catch(error => console.error(error.message))
+    .finally(() => setLoading(false))
+  }, []);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
@@ -18,7 +34,7 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys} />
     </>
   );
 }
--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({ id, name, image, likes }) {
   return (
-    <div className="card" data-testid="toy-card">
-      <h2>{"" /* Toy's Name */}</h2>
+    <li>
+      <div className="card" data-testid="toy-card">
+      <h2>{name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={image}
+        alt={name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
+      <p>{likes} Likes </p>
       <button className="like-btn">Like {"<3"}</button>
       <button className="del-btn">Donate to GoodWill</button>
-    </div>
+      </div>
+    </li>
   );
 }
 
